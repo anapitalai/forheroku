1c2f8d1b071c07ee680d30a1ff4cb6b755444a51
Alois Napitalai <anapitalai@gmail.com>
2019-07-04 15:29:04 +1000

    add CRUD

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Post,Get } from '@nestjs/common';
+import { create } from 'domain';
 
 @Injectable()
-export class PropertyService {}
+export class PropertyService {
+    @Get()
+    get(){
+        return 'all the properties around lae';
+    }
+    @Post()
+    create(){
+        return 'all the posted stuff';
+    }
+}
