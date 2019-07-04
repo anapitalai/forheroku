4db9556cecc708816fc69265325242fcfffe4a12
Alois Napitalai <anapitalai@gmail.com>
2019-07-04 15:31:51 +1000

    correction CRUD

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('property')
-export class PropertyController {}
+export class PropertyController {
+    @Get()
+    get(){
+        return 'all the properties around lae';
+    }
+    @Post()
+    create(){
+        return 'all the posted stuff';
+    }
+}
--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,14 +1,7 @@
-import { Injectable,Post,Get } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { create } from 'domain';
 
 @Injectable()
 export class PropertyService {
-    @Get()
-    get(){
-        return 'all the properties around lae';
-    }
-    @Post()
-    create(){
-        return 'all the posted stuff';
-    }
+
 }
