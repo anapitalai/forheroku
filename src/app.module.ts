cea5bcc434b8a81852a430fddc754476268106a3
Alois Napitalai <anapitalai@gmail.com>
2019-07-04 15:38:08 +1000

    add db connection string in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertyService } from './property/property.service';
@@ -6,7 +7,7 @@ import { PropertyController } from './property/property.controller';
 import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [PropertyModule],
+  imports: [MongooseModule.forRoot('mongodb://alois:raliku2019>@ds347467.mlab.com:47467/raliku'),PropertyModule],
   controllers: [AppController, PropertyController],
   providers: [AppService, PropertyService],
 })
