ae7749475e4dd33dde46c6d85173189d3a8475ca
Alois Napitalai <anapitalai@gmail.com>
2019-07-04 15:19:36 +1000

    add property endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PropertyService } from './property/property.service';
+import { PropertyController } from './property/property.controller';
+import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PropertyModule],
+  controllers: [AppController, PropertyController],
+  providers: [AppService, PropertyService],
 })
 export class AppModule {}
