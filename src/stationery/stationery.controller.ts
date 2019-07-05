04ceedad52f2de7b9bbd626f57ef4d094687b908
Alois Napitalai <anapitalai@gmail.com>
2019-07-05 13:34:28 +1000

    fix statinery endpoints

--- src/stationery/stationery.controller.ts
+++ src/stationery/stationery.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common'
 import { CreatePropertyDTO } from 'src/property/dto/property.dto';
 import { Property } from 'src/property/interface/property.interface';
 import { StationeryService } from './stationery.service';
+import { CreateStationeryDTO } from './dto/stationery.dto';
+import { Stationery } from './interface/stationery.interface';
 
 @Controller('stationery')
 export class StationeryController {
@@ -11,19 +13,19 @@ export class StationeryController {
         return this.stationeryService.get();
     }
     @Post()
-    create(@Body() createPropertyDTO:CreatePropertyDTO):Promise<Property>{
-        return this.stationeryService.create(createPropertyDTO);
+    create(@Body() createStationeryDTO:CreateStationeryDTO):Promise<Stationery>{
+        return this.stationeryService.create(createStationeryDTO);
     }
 
     @Put(':id')
     //@UseInterceptors(FileInterceptor('images'))
-    async update(@Param('id') id,@Body() updatePropertyDTO:CreatePropertyDTO):Promise<Property>{
-    return this.stationeryService.update(id,updatePropertyDTO);
+    async update(@Param('id') id,@Body() updateStationeryDTO:CreateStationeryDTO):Promise<Stationery>{
+    return this.stationeryService.update(id,updateStationeryDTO);
   }
 
 
     @Delete(':id')
-    delete(@Param('id') id):Promise<Property> {
+    delete(@Param('id') id):Promise<Stationery> {
         return this.stationeryService.delete(id);
     }
 }
